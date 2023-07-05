16e2396aa9e60fe53256091807c91ace8c780bbd
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-05 17:08:11 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@import './assets/scss/vars';
+h1 {
+  font-family: $font-primary;
+  color: $text-color-primary;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/scss/style.scss';
+import '@fontsource/bebas-neue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.vue'
+
+createApp(App).mount('#app')
